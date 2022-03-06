c4be9fe01c9b385c2e717aef34dc8b7f4f43fde6
Nikita <nikitaagarwal8755@gmail.com>
2022-03-06 15:08:10 +0530

    PlayWithArray Submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,6 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
+        let lst=[];
+        for(let i=0;i<arr.length-1;i++){
+            if(arr[i]%2==0){
+                lst.push(arr[i])
+            }
+        }
+        return lst
 
 }
 
